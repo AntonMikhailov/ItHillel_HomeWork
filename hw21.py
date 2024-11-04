2b4fa173dcf0b9362cb71084ee8b2a67f3378ae0
Tony Alexson <anton.a.mikhailov@gmail.com>
2024-11-04 19:11:07 +0200

    hw21

---
+++ hw21.py
@@ -0,0 +1,8 @@
+number = input("Enter a four-digit number: ")
+if len(number) == 4:
+    print(number[0])
+    print(number[1])
+    print(number[2])
+    print(number[3])
+else:
+    print("Error! Please enter a four-digit-number.")
