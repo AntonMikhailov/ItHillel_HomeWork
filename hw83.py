6d8ce83b8a567c6ae9e84fa827ea525d9279b227
Tony Alexson <anton.a.mikhailov@gmail.com>
2024-11-20 03:23:01 +0200

    hw8

---
+++ hw83.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def find_unique_value(some_list):
+   return [key for key, value in Counter(some_list).items() if value == 1][0]
+
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
