29cd21a6a65a26124eb7d1579f758de15a311fd7
Tony Alexson <anton.a.mikhailov@gmail.com>
2024-11-21 22:01:28 +0200

    hw83

--- hw83.py
+++ hw83.py
@@ -1,10 +1,12 @@
 from collections import Counter
 
 def find_unique_value(some_list):
-   return [key for key, value in Counter(some_list).items() if value == 1][0]
+   list_of_doubles = [key for key, value in Counter(some_list).items() if value == 1]
+   return list_of_doubles[0] if len(list_of_doubles) > 0 else None
 
 
 assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+assert find_unique_value([5, 5, 5, 2, 2]) == None, 'Test4'
 print("ОК")
